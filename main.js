5a5bdf41d23b5d4e7c6ac7e4b2b8342ac918a0dc
Faitan2010 <jenia1800@gmail.com>
2022-08-04 19:29:54 +0300

    second

---
+++ main.js
@@ -0,0 +1,27 @@
+const times = document.querySelector('.five-text');
+const input = document.querySelector('input');
+const text = document.querySelector('.text')
+let date = new Date()
+times.innerHTML = date.toLocaleTimeString('ru', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    })
+setInterval(function (){
+    let date = new Date()
+    times.innerHTML = date.toLocaleTimeString('ru', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    });
+    console.log(times.innerHTML);
+}, 1000)
+input.addEventListener('input', function (e) {
+    setInterval(function () {
+        if(input.value === '') {
+            text.classList.add('active')
+        } else {
+            text.classList.remove('active')
+        }
+    })
+})
