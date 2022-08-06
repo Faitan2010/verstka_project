fc46d1a63f4f2895f4c39fa2f553cae8d022d4ca
Faitan2010 <jenia1800@gmail.com>
2022-08-06 21:41:55 +0300

    create tips list

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const times = document.querySelector('.five-text');
 const input = document.querySelector('input');
-const text = document.querySelector('.text')
+const text = document.querySelector('.text');
+const weatherText = document.querySelector('.start-text');
+const tips = document.querySelector('.tips')
 let date = new Date()
 times.innerHTML = date.toLocaleTimeString('ru', {
     hour: '2-digit',
@@ -14,18 +16,33 @@ setInterval(function () {
         minute: '2-digit',
         second: '2-digit'
     });
-    console.log(times.innerHTML);
 }, 1000)
 
 input.addEventListener('input', function (e) {
+    tips.innerHTML = ''
     if (input.value.length < 2) {
         text.classList.add('active');
         return;
     }
     text.classList.remove('active');
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input.value}&limit=5&appid=18c5ded70673077016efa068226c43a4`)
+    .then((res)=> res.json())
+    .then((data)=> {
+        console.log(data)
+        for(let value of data) {
+            const paragraf = document.createElement('p');
+            paragraf.innerHTML = value.name;
+
+            tips.appendChild(paragraf)
+        }
+    })
+    .catch((err)=>err)
 })
 input.addEventListener('blur', ()=> {
     if (text.classList.contains('active')){
         text.classList.remove('active')
     }
 } )
+console.log(`${input.value}`)
+// document.createElement()
+// Element.uppendChild
