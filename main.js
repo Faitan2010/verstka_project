01e55b4bef531990c510afb572090b7815a88005
a.sivak <riziy_bes@mail.ru>
2022-08-11 12:55:26 +0200

    start to embed data

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const input = document.querySelector('input');
 const text = document.querySelector('.text');
 const weatherText = document.querySelector('.start-text');
 const tips = document.querySelector('.tips');
+const temper = document.querySelector('.second-text');
+const temperFeel = document.querySelector('.third-text > span');
+const sky = document.querySelector('.clouds');
+const icon = document.querySelector('.img')
 let date = new Date()
 times.innerHTML = date.toLocaleTimeString('ru', {
     hour: '2-digit',
@@ -19,9 +23,10 @@ setInterval(function () {
 }, 1000)
 
 input.addEventListener('input', function (e) {
-    tips.innerHTML = ''
+    tips.innerHTML = '';
     if (input.value.length < 2) {
         text.classList.add('active');
+        tips.classList.remove('active-tips')
         return;
     }
     text.classList.remove('active');
@@ -33,10 +38,22 @@ input.addEventListener('input', function (e) {
             const paragraf = document.createElement('p');
             paragraf.innerHTML = value.name;
             paragraf.classList.add('info')
+            tips.classList.add('active-tips')
             paragraf.addEventListener('click', (r) => {
                 fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${value.lat}&lon=${value.lon}&appid=18c5ded70673077016efa068226c43a4`)
                 .then((resOne)=> resOne.json())
-                .then((dataOne)=> console.log(dataOne))
+                .then((dataOne)=> {
+                    console.log(dataOne);
+                    if (dataOne.cod === 200) {
+                        tips.classList.remove('active-tips');
+                        input.value = '';
+                        weatherText.innerHTML = dataOne.name + ` (${dataOne.sys.country})`;
+                        temper.innerHTML = `${dataOne.main.temp}°C`;
+                        temperFeel.innerHTML = dataOne.main.feels_like;
+                        sky.innerHTML = dataOne.weather[0].description;
+                        icon.setAttribute('src', `http://openweathermap.org/img/wn/${dataOne.weather[0].icon}@2x.png`);
+                    }
+                } )
             })
             tips.appendChild(paragraf);
         }
